3a1c2a6ad85a6868c3224c6c906f87e19970df92
DaviJOFaria <davijof07@gmail.com>
2025-11-08 17:34:21 -0300

    Calculadora de determinante de matriz

--- TrabalhoAGA/MatrizDet.py
+++ TrabalhoAGA/MatrizDet.py
@@ -1,7 +1,24 @@
 import sys
 
+def laplace(matriz, xLinha, xColuna):
+    newMatriz = []
+
+    for i, linha in enumerate(matriz):
+        if i != xLinha:
+            newLinha = []
+            for j, num in enumerate(linha):
+                if j != xColuna:
+                    newLinha.append(num)
+            if newLinha:
+                newMatriz.append(newLinha)
+    return newMatriz
+                    
+            
+
+
 def calculoDet(matriz):
     ordem = len(matriz)
+
 
     if ( ordem == 1 ):
         return matriz[0][0]
@@ -14,6 +31,19 @@ def calculoDet(matriz):
         + (matriz[0][2] * matriz[1][0] * matriz[2][1] )) - ((matriz[0][1] * matriz [1][0] * matriz[2][2]) + 
         (matriz[0][0] * matriz[1][2] * matriz [2][1]) + (matriz[0][2] * matriz[1][1] * matriz[2][0])))
     
+    det = 0
+    
+    for j in range(ordem):
+        sinalCofator = (-1) ** j
+        submatriz = laplace(matriz, 0, j)
+        detSubmatriz = calculoDet(submatriz)
+
+        det += matriz[0][j] * sinalCofator * detSubmatriz
+    
+    return det
+        
+
+
 print("=== Calculo de determinantes de matriz ===")
 
 linhas = int(input("Digite a quantidade de linhas da matriz: "))
